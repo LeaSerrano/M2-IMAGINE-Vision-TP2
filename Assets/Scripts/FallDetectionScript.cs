e745f83e92c066340916bb44d494b3c283b532f2
LeaSerrano <leaserrano1119@gmail.com>
2023-09-22 15:22:41 +0200

    Initialisation du tp

---
+++ Assets/Scripts/FallDetectionScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallDetectionScript : MonoBehaviour
+{
+    public Transform Spawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "Player")
+        {
+            other.transform.position = Spawn.transform.position;
+            other.transform.rotation = Spawn.transform.rotation;
+        }
+    }
+}
